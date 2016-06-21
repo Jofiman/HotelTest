f524c044ecaa098369d7ade26785730b63770de8
Jovan Poplasen <jovanpoplasen@gmail.com>
2016-06-21 12:26:17 +0200

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -3,27 +3,37 @@ var Test = angular.module('test', []);
 Test.controller('hotelsController', ['$scope', '$http', function ($scope, $http){
     
     var id;
-    
+    var hReviews = document.getElementById('hotelReviews');
     $scope.getHotels = function(){
         $http.get('http://fake-hotel-api.herokuapp.com/api/hotels')
         .success(function(data) {
              $scope.hotels = data;
-             id = data[0].id;
-
-             $scope.getReviews = function() {
-                $http.get('http://fake-hotel-api.herokuapp.com/api/reviews?hotel_id=id')
-                .success(function(data) {
-                     $scope.reviews = data;
-
-                 })
-                .error(function(data) {
-                    $scope.reviews = 'Response failed';
-                });
-            }
-            
+             for (var i = 0; i > data.length; i++) {
+                id = data[i].id;
+             }
          })
         .error(function(data) {
             $scope.reviews = 'Response failed';
         });
+        
+        
+        
+//        if(hReviews.length > 0) {
+//           this.style.display = 'none';
+//        }
+//        else {
+//            this.style.display = 'block';
+//        }
+    }
+    $scope.getReviews = function(id) {
+        $http.get('http://fake-hotel-api.herokuapp.com/api/reviews?hotel_id='+ id)
+        .success(function(data) {
+             $scope.reviews = data;
+
+         })
+        .error(function(data) {
+            $scope.reviews = 'Response failed';
+        });
+         
     }
 }]);
